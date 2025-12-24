6f825b81d3a7e34cfb4ce3807abab4af1ae7925f
Jamiu Olanrewaju <techwithlanre@gmail.com>
2025-12-24 16:25:49 +0100

    ww

--- components/About.tsx
+++ components/About.tsx
@@ -23,7 +23,7 @@ const About: React.FC = () => {
             transition={{ duration: 0.8 }}
           >
             <img 
-              src="https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+              src="https://rrinstri.sirv.com/cloudomsystems/about.avif" 
               alt="Product Team Collaboration" 
               className="w-full object-cover grayscale hover:grayscale-0 transition-all duration-700"
               loading="lazy"
